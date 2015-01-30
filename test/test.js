b6d330673054968899c776d37757d40574af7f6a
John Otander <johnotander@gmail.com>
2015-01-30 16:03:22 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var hslaRegex = require('..');
+
+describe('hsla-regex', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(hslaRegex(), true);
+  });
+});
